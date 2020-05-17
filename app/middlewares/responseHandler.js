7e9ea6f402d7e6554daed9d2d3baa69f54c46bfa
Gabo1985 <Gabo1985@users.noreply.github.com>
2020-05-17 01:12:41 -0500

    Add files via upload

---
+++ app/middlewares/responseHandler.js
@@ -0,0 +1,41 @@
+function getPath(req){
+  let path       = req.url.split('?').shift();
+  let slashIndex = path.indexOf('/', 1);
+  if (slashIndex !== -1) {
+    path   = path.substr(0, slashIndex);
+  }
+  return path;
+}
+module.exports = function (xirsys) {
+  return [
+    (req, res, next) => {
+      console.warn('REQUEST RESPONSE - ', req.error, req.success);
+      let path = getPath(req);
+      if (path === '/_turn' && req.method === 'PUT') {
+        let v = {iceServers:xirsys.fallbackIceServers};
+        if(req.success){
+          if(req.success.v) {
+            if (req.success.v.iceServers) {
+              v.iceServers = req.success.v.iceServers;
+            }
+          }
+        }
+        req.error   = null;
+        req.success = {
+          v: v,
+          s: "ok"
+        };
+      }
+      next();
+    },
+    (req, res) => {
+      if (req.error) {
+        res.status(500).send(req.error);
+      } else {
+        if (req.success) {
+          res.send(req.success);
+        }
+      }
+    }
+  ];
+};
